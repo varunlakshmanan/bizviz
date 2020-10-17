0beb3cc07a93631b88829d11e83cf2e047c77d0f
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-10-17 02:27:56 -0400

    Added flask POST request for revenue

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request, jsonify
+from revenue_model import predict
 
 app = Flask(__name__)
 
@@ -7,7 +9,17 @@ def home():
     return "Hello World"
 
 
-
+@app.route('/getEstimatedRevenue', methods=['POST'])
+def getEstimatedRevenue():
+    file_path = request.json['file_path']
+    sector = request.json['sector']
+    advertising = request.json['advertising']
+    wages = request.json['wages']
+    fixed_costs = request.json['fixed_costs']
+    other_costs = request.json['other_costs']
+    online = request.json['online']
+    time = request.json['time']
+    return jsonify(str(predict(file_path, sector, advertising, wages, fixed_costs, other_costs, online, time)))
 
 
 if __name__ == '__main__':
