e91a8a0cfbae6eb8da1b1e2cb86b0ab7da915167
Pranav Pusarla <ppusarla@gmail.com>
2020-10-17 18:42:51 -0700

    fixed fbprophet

--- src/data/fbp.py
+++ src/data/fbp.py
@@ -18,15 +18,15 @@ def get_list_of_prices(stock_list, num_of_months):
         new_names = ['ds', 'y']
         old_names = stock_data.columns[col_indices]
         stock_data.rename(columns=dict(zip(old_names, new_names)), inplace=True)
-        print(stock_data)
+        #print(stock_data)
         stock_data = stock_data.filter(items=['ds', 'y'])
-        print(stock_data)
+        #print(stock_data)
         model = Prophet(daily_seasonality=False, weekly_seasonality=False, yearly_seasonality=True, changepoint_prior_scale=0.05, changepoints=None)
         model.add_seasonality(name='monthly', period=30.5, fourier_order=5)
         model.fit(stock_data)
 
         #makes prediction one year in advance
-        future_data = model.make_future_dataframe(periods=200)
+        future_data = model.make_future_dataframe(periods=((num_of_months+1)*30))
         forecast = model.predict(future_data)
         forecasted_price = forecast[['ds', 'yhat']]
         today = datetime.datetime.now()
@@ -65,11 +65,11 @@ def get_money(stock_list, list_of_prices, money):
         count+=1
     return dict
 
-num_of_months = 5
+#num_of_months = 10
 # low_risk_stock_list = ['TDTF', 'BIV', 'PZA']
-medium_risk_stock_list = ['GOOGL', 'URI', 'MSFT']
+#medium_risk_stock_list = ['GOOGL', 'URI', 'MSFT']
 # high_risk_stock_list = ['TSLA', 'AMZN', 'NVDA', 'AAPL']
-list_of_prices = get_list_of_prices(medium_risk_stock_list, num_of_months)
+#list_of_prices = get_list_of_prices(medium_risk_stock_list, num_of_months)
 # print(list_of_prices)
-money = get_money(medium_risk_stock_list, list_of_prices, 50)
-print(money)
+#money = get_money(medium_risk_stock_list, list_of_prices, 50)
+#print(money)
--- src/data/test.py
+++ src/data/test.py
@@ -1,7 +1,30 @@
 import requests
 import pandas as pd
+from src.ibm_api.stock_data import get_stock_data_yf
+from src.data.fbp import get_list_of_prices,get_money
 
-csv = requests.get('https://raw.githubusercontent.com/IBM/watson-stock-market-predictor/master/data/AAPL.csv')
-df = pd.read_csv('https://raw.githubusercontent.com/IBM/watson-stock-market-predictor/master/data/AAPL.csv', index_col=0)
+# csv = requests.get('https://raw.githubusercontent.com/IBM/watson-stock-market-predictor/master/data/AAPL.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/IBM/watson-stock-market-predictor/master/data/AAPL.csv', index_col=0)
+#
+# df.to_csv('AAPL.csv')
 
-df.to_csv('AAPL.csv')
+
+time = 10
+money = 40
+list = []
+low_risk_stock_list = ['TDTF', 'BIV', 'PZA']
+medium_risk_stock_list = ['GOOGL', 'URI', 'MSFT']
+high_risk_stock_list = ['TSLA', 'AMZN', 'NVDA', 'AAPL']
+get_stock_data_yf(low_risk_stock_list)
+list_of_prices = get_list_of_prices(low_risk_stock_list, time)
+low_risk = get_money(stock_list=low_risk_stock_list, list_of_prices=list_of_prices, money=money)
+list.append(low_risk)
+get_stock_data_yf(medium_risk_stock_list)
+list_of_prices = get_list_of_prices(medium_risk_stock_list, time)
+medium_risk = get_money(stock_list=medium_risk_stock_list, list_of_prices=list_of_prices, money=money)
+list.append(medium_risk)
+get_stock_data_yf(high_risk_stock_list)
+list_of_prices = get_list_of_prices(high_risk_stock_list, time)
+high_risk = get_money(stock_list=high_risk_stock_list, list_of_prices=list_of_prices, money=money)
+list.append(high_risk)
+print(list)
