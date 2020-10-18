2aff9222a80dccb15c010091bcffdac86e7932ef
Pranav Pusarla <ppusarla@gmail.com>
2020-10-18 02:34:26 -0700

    add stock

--- src/data/fbp.py
+++ src/data/fbp.py
@@ -13,7 +13,7 @@ def get_list_of_prices(stock_list, num_of_months):
     count = 1
     list_of_prices = []
     for stock in stock_list:
-        list_of_projected = []
+        list_of_projected = {}
         stock_data = df.iloc[:, count:count+7]
         count+=7
         col_indices = [0,1]
@@ -37,22 +37,28 @@ def get_list_of_prices(stock_list, num_of_months):
         today = datetime.datetime.now()
         for i in range(num_of_months):
             future = today + relativedelta(months=i + 1)
-            future = future.strftime("%Y-%m-%d")
-            list_of_projected.append(forecasted_price[forecasted_price['ds']==future].values[0][1])
+            future = future.strftime("%m-01-%Y")
+            # future += "-01"
+            value = forecasted_price[forecasted_price['ds'] == future].values[0][1]
+            future += ' GMT'
+            list_of_projected[future] = value
         list_of_prices.append(list_of_projected)
         #forecasted_price.to_csv('future_data.csv')
     return list_of_prices
 
 def predict_price(stock, model, num_of_months):
-    list_of_projected = []
+    list_of_projected = {}
     future_data = model.make_future_dataframe(periods=((num_of_months + 1) * 30))
     forecast = model.predict(future_data)
     forecasted_price = forecast[['ds', 'yhat']]
     today = datetime.datetime.now()
     for i in range(num_of_months):
         future = today + relativedelta(months=i + 1)
-        future = future.strftime("%Y-%m-%d")
-        list_of_projected.append(forecasted_price[forecasted_price['ds'] == future].values[0][1])
+        future = future.strftime("%m-01-%Y")
+        # future += "-01"
+        value = forecasted_price[forecasted_price['ds'] == future].values[0][1]
+        future += ' GMT'
+        list_of_projected[future] = value
     #list_of_prices.append(list_of_projected)
     # forecasted_price.to_csv('future_data.csv')
     return list_of_projected
@@ -62,8 +68,8 @@ def get_money(stock_list, list_of_prices, money):
     stock_money = money / len(stock_list)
     dict = {}
     count = 0
-    today = datetime.datetime.now()
-    today = today.strftime("%Y-%m-%d")
+    # today = datetime.datetime.now()
+    # today = today.strftime("%Y-%m-%d")
     for stock in stock_list:
         # share = Share(stock)
         #stock_price = yf.download(stock, start=today, end=today)
@@ -73,15 +79,16 @@ def get_money(stock_list, list_of_prices, money):
         #print(stock_price)
         num_of_shares = stock_money / float(stock_price)
         list_of_stock = list_of_prices[count]
-        i = 1
-        for price in list_of_stock:
+        #print(list_of_stock)
+        # i = 1
+        for date, price in list_of_stock.items():
             uprice = num_of_shares * price
-            s = str(i)
-            if (s in dict.keys()):
-                dict[s] = dict[s]+ uprice
+            # s = str(i)
+            if (date in dict.keys()):
+                dict[date] = dict[date]+ uprice
             else:
-                dict[s] = uprice
-            i+=1
+                dict[date] = uprice
+            #i+=1
         count+=1
     return dict
 
@@ -91,5 +98,6 @@ def get_money(stock_list, list_of_prices, money):
 #high_risk_stock_list = ['TSLA', 'AMZN', 'NVDA', 'AAPL']
 #list_of_prices = get_list_of_prices(high_risk_stock_list, num_of_months)
 #print(list_of_prices)
+#temp_list = [{'2020-11-01': 444.58105600107984, '2020-12-01': 476.3930626472659, '2021-01-01': 513.7873154199672, '2021-02-01': 551.9584350369109, '2021-03-01': 576.6159015623272}, {'2020-11-01': 3314.3813289361915, '2020-12-01': 3403.458750136123, '2021-01-01': 3471.237244499922, '2021-02-01': 3643.584211818914, '2021-03-01': 3689.151627413864}, {'2020-11-01': 549.3280722258825, '2020-12-01': 562.9468660336015, '2021-01-01': 587.3330001816589, '2021-02-01': 609.6957059514913, '2021-03-01': 621.3325014357209}, {'2020-11-01': 123.39610206550843, '2020-12-01': 124.17790857381368, '2021-01-01': 127.15492871073009, '2021-02-01': 131.13093697525866, '2021-03-01': 131.60752995602556}]
 #money = get_money(high_risk_stock_list, list_of_prices, 50)
 #print(money)
